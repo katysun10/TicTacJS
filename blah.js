35b6a612bd976331b4d44188695ea3cfc221f9bd
Katy Sun <katysun10@Katys-MacBook-Pro.local>
2017-06-19 11:50:07 -0600

    first commit

---
+++ blah.js
@@ -0,0 +1,39 @@
+
+Player = function(){
+	this.name = "";
+	this.symbol = "";
+}
+
+var playerOne = new Player();
+var playerTwo = new Player();
+
+var readline = require('readline'),
+rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.question("Player 1, what is your name? ", function(name1){
+	playerOne.name = name1;
+
+	rl.question("Player 2, what is your name? ", function(name2){
+		playerTwo.name = name2;
+
+		rl.question("Enter a symbol to use as your game piece "+playerOne.name+": ", function(symbol1){
+			playerOne.symbol = symbol1;
+
+			rl.setPrompt("Enter a symbol to use as your game piece "+playerTwo.name+": ");
+			rl.prompt();
+
+			rl.on('line', function(symbol2){
+				if(symbol2 === symbol1){
+					console.log("Player one and two cannot have the same symbol "+playerTwo.name+"!")
+					rl.prompt();
+				}
+				else{
+					playerTwo.symbol = symbol2;
+					rl.close();
+					console.log(playerOne);
+					console.log(playerTwo);
+				}
+			});
+		});
+	});
+});
