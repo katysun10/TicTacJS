78c5fd6ff951b37e9afae7bfb8074a628463078b
Katy Sun <katysun10@Katys-MacBook-Pro.local>
2017-06-21 16:45:21 -0600

    other update

--- gameboard.js
+++ gameboard.js
@@ -1,7 +1,4 @@
 
-var readline = require('readline'),
-rl = readline.createInterface(process.stdin, process.stdout);
-
 function GameBoard(){
 	this.numRows;
 	this.numCols;
@@ -16,21 +13,6 @@ function GameBoard(){
         	tempArr = [];
     	}
 	}
-}
-
-function setUpBoard(){
-    var board = new GameBoard();
-    rl.question("How many rows? (Enter a 3) ", function(rows){
-        board.numRows = rows;
-        rl.question("How many columns? (Enter a 3) ", function(columns){
-            board.numCols = columns;
-            console.log("Your board has "+board.numRows+" rows, and "+board.numCols+" columns");
-            console.log("");
-            board.buildBoard();
-            board.getBoard();
-            rl.pause();
-         });
-    });
 }
 
 exports.GameBoard = GameBoard;
